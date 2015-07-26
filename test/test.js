6ec5fda57d4daef906db3519f05d33903ee7a369
Adrian Perez de Castro <aperez@igalia.com>
2015-07-26 15:13:35 +0300

    Improve hipack.dump() test cases
    - Add test case for encoding objects recursively.
    - Make test descriptions shorter.

--- test/test.js
+++ test/test.js
@@ -11,31 +11,37 @@ var should = require("should");
 describe("hipack", function () {
 	describe(".dump()", function () {
 
-		it("should encode empty objects", function () {
+		it("encodes empty objects", function () {
 			hipack.dump({}).should.equal("");
 		});
-		it("should encode integer numbers", function () {
+		it("encodes integer numbers", function () {
 			hipack.dump({ value: 42 }).should.equal("value: 42\n");
 		});
-		it("should encode floating point numbers", function () {
+		it("encodes floating point numbers", function () {
 			hipack.dump({ value: 1.5 }).should.equal("value: 1.5\n");
 		});
-		it("should encode string values", function () {
+		it("encodes string values", function () {
 			hipack.dump({ value: "a string" }).should.equal(
 					"value: \"a string\"\n");
 		});
-		it("should encode boolean values", function () {
+		it("encodes boolean values", function () {
 			hipack.dump({ value: false }).should.equal("value: False\n");
 			hipack.dump({ value: true }).should.equal("value: True\n");
 		});
-		it("should encode empty arrays", function () {
+		it("encodes empty arrays", function () {
 			hipack.dump({ value: [] }).should.equal("value []\n");
 		});
-		it("should encode empty objects recursively", function () {
+		it("encodes empty objects recursively", function () {
 			hipack.dump({ value: {} }).should.equal("value {}\n");
 		});
+		it("encodes objects recursively", function () {
+			hipack.dump({ value: { value: 42 } }).should.equal(
+					"value {\n" +
+					"  value: 42\n" +
+					"}\n");
+		});
 
-		it("should sort dictionary keys", function () {
+		it("sorts dictionary keys", function () {
 			hipack.dump({ c:1, b:2, a:3 }).should.equal("a: 3\nb: 2\nc: 1\n");
 		});
 
